99bf08facf96d24fd59dee81c471b89fe6594e76
Rob <robert.cook@bcu.ac.uk>
2023-01-11 15:02:38 +0000

    Update config write

--- db_setup.R
+++ db_setup.R
@@ -1,4 +1,17 @@
 library(keyring)
 
+if(!file.exists("config.yml")){
+  
+  config.deafults <- c(
+    "default:",
+    "  Driver: SQL Server",
+    "  Server: ",
+    "  Database: ",
+    "  UID: !expr keyring::key_get('db_credentials_UID')",
+    "  PWD: !expr keyring::key_get('db_credentials_PWD')"
+  )
+  write(config.deafults, "config.yml")
+}
+
 key_set("db_credentials_UID", prompt = "NuRS database username: ")
 key_set("db_credentials_PWD", prompt = "NuRS database password: ")
